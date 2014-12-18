c08200cf89c8559212d30294b315a1fd4f2e0f9c
ruipedrovb <ruipedrovb@gmail.com>
2014-12-18 15:30:54 +0000

    Adds routes.rb and inflections

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  defaults format: :json do
-    constraints format: :json do
-      resources :rules
-
-      resources :wallets
-
-      mount_devise_token_auth_for 'Account', at: '/auth'
-      
-      resources :children
-
-      resources :managers
-    end
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api', format: :json }, path: '/' do
+    resources :rules
+    resources :wallets
+    mount_devise_token_auth_for 'Account', at: '/auth'
+    resources :children
+    resources :managers
   end
 end
